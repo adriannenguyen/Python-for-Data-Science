23da443077b07566e836dd4973e484cfafa87585
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-13 20:37:24 +0000

    review questions

---
+++ Module 1/reviewquestions.py
@@ -0,0 +1,19 @@
+# What is the result of the following operation in Python:
+print(3 + 2 * 2)
+
+# In Python, if you executed name = 'Lizz', what would be the output of print(name[0:2])?
+name = "Lizz"
+print(name[0:2])
+
+# In Python, if you executed var = '01234567', what would be the result of print(var[::2])?
+var = "01234567"
+print(var[::2])
+
+# In Python, what is the result of the following operation '1'+'2'?
+print("1" + "2")
+
+# Given myvar = 'hello', how would you convert myvar into uppercase?
+myvar = "hello"
+print(myvar.upper())
+
+
