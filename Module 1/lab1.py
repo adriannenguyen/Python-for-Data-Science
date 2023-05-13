c94e2e3fc8f1cecf8ed6b4feb6f0c9b99764168e
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-13 05:40:31 +0000

    lab 1

---
+++ Module 1/lab1.py
@@ -0,0 +1,36 @@
+"""Exercise: Your First Program"""
+# In the code cell below, use the print() function to print out the phrase: Hello, world!
+print("Hello, world!")
+
+# In the code cell below, print out the phrase: Hello, world!
+# and comment it with the phrase Print the traditional hello world all in one line of code.
+
+print("Hello, world!") # Print the traditional hello world
+
+# Types of objects in Python
+
+# In the code cell below, use the type() function to check the object type of 12.0.
+print(type(12.0))
+
+# Exercise: Types
+# What is the data type of the result of: 6 / 2?
+print(type(6/2))
+
+# What is the type of the result of: 6 // 2? (Note the double slash //.)
+print(type(6//2))
+
+# Exercise: Expression
+# Let's write an expression that calculates how many hours there are in 160 minutes:
+print(160//60)
+
+# Exercise: Expression and Variables in Python
+# What is the value of x where x = 3 + 2 * 2
+x = (3 + 2 * 2)
+print(x)
+
+# What is the value of y where y = (3 + 2) * 2?
+y = (3 + 2) * 2
+print(y)
+
+# What is the value of z where z = x + y?
+print(x + y)
--- Module 1/pythontypes.py
+++ Module 1/pythontypes.py
@@ -1,5 +1,6 @@
+"""Types"""
 # What is the type of the following: int(1.0)?
 print(type(int(1.0)))
 
-#Enter the code to convert the number 1 to a Boolean.
+# Enter the code to convert the number 1 to a Boolean.
 print(type(bool(1)))
