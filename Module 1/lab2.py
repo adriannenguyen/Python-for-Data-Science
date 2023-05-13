021b809c36e75eedd181422118766fc7f89452d1
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-13 06:47:50 +0000

    lab 2

---
+++ Module 1/lab2.py
@@ -0,0 +1,11 @@
+# What is the value of the variable a after the following code is executed?
+a = "1"
+print(a)
+
+# What is the value of the variable b after the following code is executed?
+b = "2"
+print(b)
+
+# What is the value of the variable c after the following code is executed?
+c = a + b
+print(c)
---
+++ Module 1/stringoperations.py
@@ -0,0 +1,6 @@
+# How would you obtain the even elements?
+Numbers = "0123456"
+print(Numbers[::2])
+
+# What is the result of the following line of code:
+print("0123456".find('1'))
