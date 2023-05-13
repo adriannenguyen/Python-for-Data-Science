131372ceca45451750f53cd1f2e14262461f0626
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-13 06:08:03 +0000

    lab 1

--- Module 1/lab1.py
+++ Module 1/lab1.py
@@ -7,25 +7,25 @@ print("Hello, world!")
 
 print("Hello, world!") # Print the traditional hello world
 
-# Types of objects in Python
+"""Types of objects in Python"""
 
 # In the code cell below, use the type() function to check the object type of 12.0.
 print(type(12.0))
 
-# Exercise: Types
+"""Exercise: Types"""
 # What is the data type of the result of: 6 / 2?
 print(type(6/2))
 
 # What is the type of the result of: 6 // 2? (Note the double slash //.)
 print(type(6//2))
 
-# Exercise: Expression
+"""Exercise: Expression"""
 # Let's write an expression that calculates how many hours there are in 160 minutes:
 print(160//60)
 
-# Exercise: Expression and Variables in Python
+"""Exercise: Expression and Variables in Python"""
 # What is the value of x where x = 3 + 2 * 2
-x = (3 + 2 * 2)
+x = 3 + 2 * 2
 print(x)
 
 # What is the value of y where y = (3 + 2) * 2?
