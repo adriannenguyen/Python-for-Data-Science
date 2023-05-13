8162a0a845e77148d5f2c2e5428ea3e2b680a011
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-13 23:03:21 +0000

    lab 1

---
+++ Module 2/liststuples.py
@@ -0,0 +1,14 @@
+# Create a tuple  (0,1,2,3)  and assign it to variable A:
+A = (0,1,2,3)
+print(A)
+
+# Find the first two elements of tuple  A :
+print(A[0:2])
+
+# Find the first two elements of the list  B:
+B=["a","b","c"]
+print(B[0:2])
+
+# Change the first element of the list to an uppercase "A".
+B[0] = "A"
+print(B)
