679451e5058b45befebc2ce339d8141079beb8b1
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-15 21:59:07 +0000

    loops

---
+++ Module 3/loops.py
@@ -0,0 +1,13 @@
+# Use loops to print out the elements in the list A:
+
+A=[3,4,5]
+
+for i in A:
+    print(i)
+
+# Find the value of  x  that will print out the sequence  1,2,..,10 :
+x=11
+y=1
+while(y != x):
+    print(y)
+    y=y+1
