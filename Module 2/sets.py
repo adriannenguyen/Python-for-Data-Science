95ff62228a11af20555fe1ea68546941d5a5c831
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-15 00:58:30 +0000

    sets

---
+++ Module 2/sets.py
@@ -0,0 +1,13 @@
+# Cast the following list to a set:
+set(['A','B','C','A','B','C'])
+
+# Add the string 'D' to the set S.
+S = {'A','B','C'}
+S.add("D")
+print(S)
+
+# Find the intersection of set A and B.
+A = {1,2,3,4,5}
+B = {1,3,9, 12}
+C = A & B
+print(C)
