11e1cd7c10d48df167187b925988e50464e0e4c3
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-16 01:50:44 +0000

    functions

---
+++ Module 3/functions.py
@@ -0,0 +1,10 @@
+# Complete the function g such that the input c is a list of integers 
+# and the output is the sum of all the elements in the list.
+
+c = [1, 2, 3, 4, 5]
+
+def g(c):
+    return sum(c)
+
+print(c)
+
