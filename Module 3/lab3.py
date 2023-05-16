61648de24e86fe808c1e870ac6d20eb11c05b8fc
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-16 02:46:29 +0000

    lab 3

---
+++ Module 3/lab3.py
@@ -0,0 +1,10 @@
+# Come up with a function that divides the first input by the second input:
+
+def div(a, b):
+    return a/b
+
+result = div(10, 5)
+print(result)
+
+# Use the function con for the following question.
+
