35e0506f71f8c7ac651ff48f4026bf7a30f0d642
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-19 00:01:35 +0000

    numpy 1D arrays

---
+++ Module 5/numpy1D.py
@@ -0,0 +1,15 @@
+import numpy as np
+a=[1,2,3,4,5]
+x = np.array(a)
+
+# Find the type of x using the function type().
+print(type(x))
+
+# Find the shape of the array:
+print(x.shape)
+
+# Find the type of data in the array:
+print(x.dtype)
+
+# Find the mean of the array:
+print(x.mean())
