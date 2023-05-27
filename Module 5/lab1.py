f92b70610a4a8df16608eaa8d6a20a589565bfd1
Ady <125761440+adriannenguyen@users.noreply.github.com>
2023-05-27 04:04:09 +0000

    lab 1

---
+++ Module 5/lab1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Check the type of the array and Value type for the given array c
+b = np.array([3.1, 11.02, 6.2, 213.2, 5.2])
+print(type(b))
+print(b.dtype)  
+
+# Assign the value 20 for the second element in the given array.
+a = np.array([10, 2, 30, 40,50])
+a[1] = 20
+print(a)
